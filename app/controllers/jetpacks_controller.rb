69c65ebb956471d793725d0d25d64e11ef4c26ca
Benjamin Wainer <benjawainer@gmail.com>
2023-07-15 17:11:03 +0900

    Added new and create actions to jetpacks controller, and page for new jetpacks. Also added links to navigate through these pages in the meanwhile.

--- app/controllers/jetpacks_controller.rb
+++ app/controllers/jetpacks_controller.rb
@@ -1,9 +1,34 @@
 class JetpacksController < ApplicationController
+  before_action :set_jetpack, only: [:show]
+
   def index
     @jetpacks = Jetpack.all
   end
 
   def show
     @jetpack = Jetpack.find(params[:id])
+  end
+
+  def new
+    @jetpack = Jetpck.new
+  end
+
+  def create
+    @jetpack = Jetpack.new(jetpack_params)
+    if @jetpack.save
+      redirect_to index_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_jetpack
+    @jetpack = Jetpack.find(params[:id])
+  end
+
+  def jetpack_params
+    params.require(:jetpack).permit(:title, :model, :price, :description)
   end
 end
