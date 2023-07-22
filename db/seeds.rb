f8aa23fa9ef8c841865a3c1951291f28091f312f
Christian Stprix <christprix@gmail.com>
2023-07-22 14:35:44 +0900

    made seeds using cloudinary pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,9 +7,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts "Destroying all users"
-User.destroy_all
-puts "Done!"
+# puts "Destroying all users"
+# User.destroy_all
+# puts "Done!"
 
 puts "Creating 5 users"
 5.times do
@@ -22,18 +23,92 @@ end
 puts "Finished seeding users"
 
 
-puts "Destroying all jetpacks"
-Jetpack.destroy_all
-puts "Done!"
+# puts "Destroying all jetpacks"
+# Jetpack.destroy_all
+# puts "Done!"
 
-10.times do
-  jetpack = Jetpack.create!(
-    title: Faker::Superhero.name,
-    model: Faker::Science.tool,
-    price: rand(100.300),
-    description: "Exciting jetpack description",
-    user_id: rand(1..5)
-  )
-  puts "Creating jetpack number #{jetpack.id}"
-end
-puts "Finished seeding jetpacks"
+# 10.times do
+jetpack = Jetpack.new(
+  title: "Helicopter from Hell",
+  model: Faker::Science.tool,
+  price: rand(100.300),
+  description: "Ride like a helicopter...from hell",
+  user_id: rand(1..5)
+)
+file = URI.open("https://res.cloudinary.com/dm54zi0ff/image/upload/v1689997098/helicopter-jetpack_slntzl.webp")
+jetpack.photo.attach(io: file, filename: "helicopter.png", content_type: "image/png")
+jetpack.save
+puts "Creating jetpack number #{jetpack.id}"
+
+jetpack = Jetpack.new(
+  title: "Miami Style",
+  model: Faker::Science.tool,
+  price: rand(100.300),
+  description: "Fly around like a dolphin straight out of Miami beach",
+  user_id: rand(1..5)
+)
+file = URI.open("https://res.cloudinary.com/dm54zi0ff/image/upload/v1689997098/miami-special_giar9n.jpg")
+jetpack.photo.attach(io: file, filename: "miami.png", content_type: "image/png")
+jetpack.save
+puts "Creating jetpack number #{jetpack.id}"
+
+jetpack = Jetpack.new(
+  title: "Advanced Warfighter Jetpack",
+  model: Faker::Science.tool,
+  price: rand(100.300),
+  description: "Ever played Call of Duty Advanced warfighter?",
+  user_id: rand(1..5)
+)
+file = URI.open("https://res.cloudinary.com/dm54zi0ff/image/upload/v1689997086/advanced-jetpack_ofpgqi.jpg")
+jetpack.photo.attach(io: file, filename: "advanced.png", content_type: "image/png")
+jetpack.save
+puts "Creating jetpack number #{jetpack.id}"
+
+jetpack = Jetpack.new(
+  title: "The Squatter",
+  model: Faker::Science.tool,
+  price: rand(100.300),
+  description: "Sit down, relax, and fly with this jetpack!",
+  user_id: rand(1..5)
+)
+file = URI.open("https://res.cloudinary.com/dm54zi0ff/image/upload/v1689997086/sitting-jetpack_cm9yil.jpg")
+jetpack.photo.attach(io: file, filename: "squatter.png", content_type: "image/png")
+jetpack.save
+puts "Creating jetpack number #{jetpack.id}"
+
+
+jetpack = Jetpack.new(
+  title: "Water Glider",
+  model: Faker::Science.tool,
+  price: rand(100.300),
+  description: "Ariel said she wanted human legs. This will make her wish she had fins again",
+  user_id: rand(1..5)
+)
+file = URI.open("https://res.cloudinary.com/dm54zi0ff/image/upload/v1689997086/water-jetpack_lpes1y.jpg")
+jetpack.photo.attach(io: file, filename: "footer.png", content_type: "image/png")
+jetpack.save
+puts "Creating jetpack number #{jetpack.id}"
+
+jetpack = Jetpack.new(
+  title: "El Classico",
+  model: Faker::Science.tool,
+  price: rand(100.300),
+  description: "A jetpack. What else could you want?",
+  user_id: rand(1..5)
+)
+file = URI.open("https://res.cloudinary.com/dm54zi0ff/image/upload/v1689997086/classic-jetpack_ifmkfx.webp")
+jetpack.photo.attach(io: file, filename: "classic.png", content_type: "image/png")
+jetpack.save
+puts "Creating jetpack number #{jetpack.id}"
+
+jetpack = Jetpack.new(
+  title: "Mando",
+  model: Faker::Science.tool,
+  price: rand(100.300),
+  description: "Baby Yoda would be proud",
+  user_id: rand(1..5)
+)
+file = URI.open("https://res.cloudinary.com/dm54zi0ff/image/upload/v1690003889/mando_ckt07b.jpg")
+jetpack.photo.attach(io: file, filename: "mando.png", content_type: "image/png")
+jetpack.save
+puts "Creating jetpack number #{jetpack.id}"
