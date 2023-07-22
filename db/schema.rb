db9657958fe69ec1c533ca020f74c196a2033fc2
Benjamin Wainer <benjawainer@gmail.com>
2023-07-22 12:38:36 +0900

    Added dependency of jetpacks to user. Also added migration to make status column of bookings table to have default value of pending.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_201452) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_22_032409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,7 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_201452) do
 
   create_table "bookings", force: :cascade do |t|
     t.date "booking_date"
-    t.string "status"
+    t.string "status", default: "pending"
     t.string "comment"
     t.bigint "jetpack_id", null: false
     t.bigint "user_id", null: false
